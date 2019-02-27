392e7efa7b32820464cb180d93f87402a87f96b5
Nancy Badillo <n_badillo@csu.fullerton.edu>
2019-02-27 12:47:06 -0800

    django set up
    Set up django with basic index, about, register and login pages and their respective paths and views.

---
+++ ProjectCoin/ProjectCoin/pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+def register(request):
+    return render(reuqest, "register.html",{})
+
+def login(request):
+    return render(request, "login.html", {})
+
+def about(request):
+    return render(reuqest, "about.html",{})
