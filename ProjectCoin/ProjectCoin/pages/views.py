ee4d564d90e78e5289656cb9326beb5c30ab6c30
AaronP <tphan106@csu.fullerton.edu>
2019-03-02 10:13:58 -0800

    Add files via upload

--- ProjectCoin/ProjectCoin/pages/views.py
+++ ProjectCoin/ProjectCoin/pages/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .verify import customerForm
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
 
 def register(request):
-    return render(request, "register.html",{})
+    if request.method == 'POST':
+        verification = customerForm(request.POST)
+        if verification.is_valid():
+            verification.save()
+            return redirect('register')
+        else:
+            verification =customerForm()
+    return render(request,'register.html',{})
 
 def login(request):
     return render(request, "login.html", {})
