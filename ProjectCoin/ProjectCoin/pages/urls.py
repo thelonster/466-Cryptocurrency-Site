a92cf42c150843a3d2906be24c7658ec16655a27
Nancy Badillo <n_badillo@csu.fullerton.edu>
2019-03-04 20:40:51 -0800

    Update pages.py to include dashboard.html

--- ProjectCoin/ProjectCoin/pages/urls.py
+++ ProjectCoin/ProjectCoin/pages/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('bitcion/', views.bitcoin, name="bitcoin"),
     path('litecoin/', views.litecoin, name="litecoin"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
 
