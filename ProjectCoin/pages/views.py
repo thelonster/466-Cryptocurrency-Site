225360eaf4ffc4f89a7c8f25b3752016726129a5
AaronP <tphan106@csu.fullerton.edu>
2019-03-05 17:53:00 -0800

    Add files via upload

--- ProjectCoin/pages/views.py
+++ ProjectCoin/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from .verify import customerForm
 
 # Create your views here.
@@ -19,6 +20,16 @@ def register(request):
     return render(request,'register.html',{})
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = auth.authenticate(email = email, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            return redirect('login.html')
     return render(request, "login.html", {})
 
 def about(request):
@@ -30,3 +41,7 @@ def bitcoin(request):
 def litecoin(request):
     return render(request, "litecoin.html",{})
 
+def dashboard(request):
+    if not request.user.is_authenicated():
+        return redirect('login.html')
+    return render(request, "dashboard.html", {})
