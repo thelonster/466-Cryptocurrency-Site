a335aafad4f71d3f74b7111388a4a50a4d410f88
thelonster <lg3hansen@gmail.com>
2019-03-12 19:50:06 -0700

    Revert "Revert "Merge branch 'master' into register""
    This reverts commit 71102dd1986b29fc55c5b6f2b3d739ce327b09fe.

--- ProjectCoin/ProjectCoin/pages/views.py
+++ ProjectCoin/ProjectCoin/pages/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .verify import customerForm
+from django.http import JsonResponse
+from datetime import datetime, timezone
+from .graph import get_df
 
 # Create your views here.
 def index(request):
@@ -30,3 +33,79 @@ def bitcoin(request):
 def litecoin(request):
     return render(request, "litecoin.html",{})
 
+def ethereum(request):
+    return render(request, "ethereum.html",{})
+
+def dashboard(request):
+    return render(request, "dashboard.html",{})
+
+def get_crypto_data(request, *args):
+    coin = request.get_full_path()
+    coin = coin[-3:]
+    data = get_df(1546290000, 1560000000, coin, "day")
+    return JsonResponse(data, safe=False)
+
+def get_year_data(request, *args):
+    coin = request.get_full_path()
+    # Coin type appended to url
+    coin = coin[-3:]
+    today = datetime.today()
+    # Retrieving Unix Epoch time for get_df function
+    timestamp = today.timestamp()
+    timestamp = int(timestamp)
+    lastyear = timestamp - 86400 * 365
+    # "day" passed an argument to get daily data from CryptoCompare
+    data = get_df(lastyear, timestamp, coin, "day")
+    return JsonResponse(data, safe=False)
+
+def get_month_data(request, *args):
+    coin = request.get_full_path()
+    # Coin type appended to url
+    coin = coin[-3:]
+    today = datetime.today()
+    # Retrieving Unix Epoch time for get_df function
+    timestamp = today.timestamp()
+    timestamp = int(timestamp)
+    lastmonth = timestamp - 86400 * 30
+    # "day" passed an argument to get daily data from CryptoCompare
+    data = get_df(lastmonth, timestamp, coin, "day")
+    return JsonResponse(data, safe=False)
+
+def get_week_data(request, *args):
+    coin = request.get_full_path()
+    # Coin type appended to url
+    coin = coin[-3:]
+    today = datetime.today()
+    # Retrieving Unix Epoch time for get_df function
+    timestamp = today.timestamp()
+    timestamp = int(timestamp)
+    lastweek = timestamp - 86400 * 7
+    # "day" passed an argument to get daily data from CryptoCompare
+    data = get_df(lastweek, timestamp, coin, "day")
+    return JsonResponse(data, safe=False)
+
+def get_day_data(request, *args):
+    coin = request.get_full_path()
+    # Coin type appended to url
+    coin = coin[-3:]
+    today = datetime.today()
+    # Retrieving Unix Epoch time for get_df function
+    timestamp = today.timestamp()
+    timestamp = int(timestamp)
+    yesterday = timestamp - 86400
+    # "hour" passed an argument to get hourly data from CryptoCompare
+    data = get_df(yesterday, timestamp, coin, "hour")
+    return JsonResponse(data, safe=False)
+
+def get_hour_data(request, *args):
+    coin = request.get_full_path()
+    # Coin type appended to url
+    coin = coin[-3:]
+    today = datetime.today()
+    # Retrieving Unix Epoch time for get_df function
+    timestamp = today.timestamp()
+    timestamp = int(timestamp)
+    lasthour = timestamp - 3600
+    # "minute" passed an argument to get minute data from CryptoCompare
+    data = get_df(lasthour, timestamp, coin, "minute")
+    return JsonResponse(data, safe=False)
