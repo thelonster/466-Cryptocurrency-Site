ec9cb85d83956e08a5252ccf54a911e61614e35d
AaronP <tphan106@csu.fullerton.edu>
2019-04-18 17:59:28 -0700

    Add files via upload

--- ProjectCoin/ProjectCoin/pages/models.py
+++ ProjectCoin/ProjectCoin/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django import forms
 # Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+    
